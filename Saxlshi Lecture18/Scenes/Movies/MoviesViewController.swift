256b07eb7c9c2986b760b0cdbd1f7479ff5ce3af
gog1na <82454907+gog1na@users.noreply.github.com>
2023-01-13 12:48:39 +0400

    Setup project files

--- Saxlshi Lecture18/Scenes/Movies/MoviesViewController.swift
+++ Saxlshi Lecture18/Scenes/Movies/MoviesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MoviesViewController.swift
 //  Saxlshi Lecture18
 //
 //  Created by Giorgi Goginashvili on 1/13/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MoviesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
